04512f6c51ca55e6c5c91dbb94471b4f295bb66b
Thiago de Jesus Santos <1051770@senacminas.edu.br>
2026-02-27 11:53:02 -0300

    Segunda Alteração

---
+++ lista.py
@@ -0,0 +1,7 @@
+lista_nomes = ['Ana', 'Beatriz', 'Craudia', 'Danilo', 'Elias', 'Fernando', 'Gabriel', 'Hiago', 'Ian', 'Juliano']
+
+lista_nomes.insert(1,"Bruna")
+lista_nomes.insert(2,"Carlos")
+lista_nomes.insert(3,"Daniel")
+
+print(lista_nomes)
---
+++ tupla.py
@@ -0,0 +1,4 @@
+pessoa = ("Thiago de Jesus", 21, "3500")
+
+nome, idade, pretensao_salarial = pessoa
+print(f"{nome} tem {idade} e a pretensão salarial de ${pretensao_salarial}")
